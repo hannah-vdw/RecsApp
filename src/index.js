281f8e1ea1c2ea07be44dea8f9f4c08f1ca78424
Hannah van den Wijngaard <h.vandenwijngaard@elsevier.com>
2022-01-28 11:19:39 +0000

    set up mongoose

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const express = require( "express" );
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConnection')
+
 const app = express();
 const port = 8080;
+
+connectDB();
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
 
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${port}` );
-} );
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo db')
+    
+    // start the Express server
+    app.listen(port, () => {
+        console.log( `server started at http://localhost:${port}` );
+    } );
+})
+
